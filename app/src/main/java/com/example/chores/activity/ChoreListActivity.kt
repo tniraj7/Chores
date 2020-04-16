541e7e90750cb24526182ae213fa5724d6315612
Niraj Tiwari <tniraj7@gmail.com>
2020-04-17 00:06:16 +0900

    add chores list activity

---
+++ app/src/main/java/com/example/chores/activity/ChoreListActivity.kt
@@ -0,0 +1,13 @@
+package com.example.chores.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.chores.R
+
+class ChoreListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chore_list)
+    }
+}
