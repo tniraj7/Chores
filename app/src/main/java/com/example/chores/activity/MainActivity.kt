ad8c3679dd2b86cbce592e87866150a66a57b384
Niraj Tiwari <tniraj7@gmail.com>
2020-04-18 02:50:09 +0900

    make progresss bar while saving Chore record in DB, upgrade git-ignore file

--- app/src/main/java/com/example/chores/activity/MainActivity.kt
+++ app/src/main/java/com/example/chores/activity/MainActivity.kt
@@ -6,35 +6,36 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
-import android.view.View
-import android.widget.ProgressBar
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.chores.R
+import com.example.chores.data.ChoreListAdapter
 import com.example.chores.data.ChoresDatabaseHandler
 import com.example.chores.model.Chore
+import kotlinx.android.synthetic.main.activity_chore_list.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
-    var dbHandler: ChoresDatabaseHandler? = null
-    var progressDialog: ProgressDialog? = null
+    private var dbHandler: ChoresDatabaseHandler? = null
+    private var progressDialog: ProgressDialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
         dbHandler = ChoresDatabaseHandler(this)
-        setContentView(R.layout.activity_main)
         progressDialog = ProgressDialog(this)
 
         saveChoreButtonId.setOnClickListener {
             progressDialog?.setMessage("Saving ..")
-            progressDialog?.show()
 
             if (!TextUtils.isEmpty(enterChoreId.text.toString())
                 && !TextUtils.isEmpty(assignedById.text.toString())
                 && !TextUtils.isEmpty(assignToId.text.toString())
             ) {
+                progressDialog?.show()
 
                 var chore = Chore()
                 chore.choreName = enterChoreId.text.toString()
