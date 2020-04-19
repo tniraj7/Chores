f504d3250eaa6485912abd8108d88e1669681339
Niraj Tiwari <tniraj7@gmail.com>
2020-04-19 13:47:08 +0900

    display popup dialog to add new chore

--- app/src/main/java/com/example/chores/activity/ChoreListActivity.kt
+++ app/src/main/java/com/example/chores/activity/ChoreListActivity.kt
@@ -1,10 +1,13 @@
 package com.example.chores.activity
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.chores.R
@@ -12,6 +15,7 @@ import com.example.chores.data.ChoreListAdapter
 import com.example.chores.data.ChoresDatabaseHandler
 import com.example.chores.model.Chore
 import kotlinx.android.synthetic.main.activity_chore_list.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class ChoreListActivity : AppCompatActivity() {
 
@@ -19,6 +23,8 @@ class ChoreListActivity : AppCompatActivity() {
     private var choreList: ArrayList<Chore>? = null
     private var choreListItems: ArrayList<Chore>? = null
     private var layoutManager: RecyclerView.LayoutManager? = null
+    private var dialogBuilder: AlertDialog.Builder? = null
+    private  var dialog: AlertDialog? = null
 
     private var dbHandler: ChoresDatabaseHandler? = null
 
@@ -60,7 +66,24 @@ class ChoreListActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.add_menu_button) {
             Log.d("Item Clicekd", "Yes")
+            createPopupDialog()
         }
+
         return super.onOptionsItemSelected(item)
+    }
+
+    fun createPopupDialog() {
+
+        var view = layoutInflater.inflate(R.layout.popup, null)
+
+        var choreName = view.enterChoreId
+        var choreAssignedBy = view.assignedById
+        var assignedTo = view.assignToId
+
+        var saveBtn = view.saveChoreButtonId
+
+        dialogBuilder = AlertDialog.Builder(this).setView(view)
+        dialog = dialogBuilder!!.create()
+        dialog!!.show()
     }
 }
